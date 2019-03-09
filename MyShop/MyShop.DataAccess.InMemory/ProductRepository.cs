2a7fd6b9706d7cfd58b423783a687f2c87a8f3c2
Joseph Rodriguez <Jrodriguez@quantumgroup.com>
2019-03-08 21:16:07 -0600

    Product Manager Pages built and tested

--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using MyShop.Core.Models;
 
 namespace MyShop.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;
@@ -46,7 +46,7 @@ namespace MyShop.DataAccess.InMemory
             }
         }
 
-        public void Find(string Id)
+        public Product Find(string Id)
         {
             Product product = products.Find(p => p.Id == Id);
 
